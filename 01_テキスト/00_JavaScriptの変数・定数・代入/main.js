32aa979522720100f0c1c51726766b34c32262ce
junseinagao <junsei.nagao@gmail.com>
2023-09-25 16:34:14 +0900

    ✅ 変数の教材を新しくしました

--- 01_テキスト/00_JavaScriptの変数・定数・代入/main.js
+++ 01_テキスト/00_JavaScriptの変数・定数・代入/main.js
@@ -1,31 +1,9 @@
-// * let キーワードを使って、hensu という名前の変数を宣言する
-let hensu
-console.log(hensu) // => undefined というなにもないことを表す値が表示される
+let myNickname = "エキスパくん"
 
-// 変数には値を代入（割り当て）できる
-hensu = 42
-console.log(hensu) // => 42
+console.log(myNickname) // => "エキスパくん" と表示される
 
-// 変数には値を再代入できる
-hensu = 100
-console.log(hensu) // => 100
+// TRY: 変数 myNickname に再代入して自分のあだ名を表示してください
 
-// * 多くの場合、変数には宣言と同時に値を代入する
-let hensu2 = 42
-console.log(hensu2) // => 42
+console.log(myNickname)
 
-// * const キーワードを使うと、定数になる
-const teisu = 42
-
-// 定数には値を再代入できない
-teisu = 100 // エラー
-console.log(teisu) // => 42
-
-// * 変数や定数の名前はだいたい自由に決められるが、-（ハイフン）が使えなかったり、最初の文字に数字が使えなかったり、あらかじめ使えないと決められた名前は使えなかったりする。
-// let my-name // だめ
-// let 1banme // だめ
-// let delete // だめ
-
-// 複数の単語を組み合わせるときは、ふたつめ以降の単語の最初の文字を大文字にしてくっつけることが多い
-// このような書き方をキャメルケースという（大文字がキャメル＝ラクダのこぶにみえることから）
-let camelCase // camel + case
+// TRY: myAge を定数で宣言して自分の年齢を表示してください
