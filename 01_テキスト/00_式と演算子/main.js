691060ff2fa1a98cce06409c36e0ea4521330243
junseinagao <junsei.nagao@gmail.com>
2023-10-07 01:43:28 +0900

    「式と演算子」を追加しました

---
+++ 01_テキスト/00_式と演算子/main.js
@@ -0,0 +1,13 @@
+// TRY: 練習問題３
+let happiness = 0; // 0 から 100 までの数値を代入してください
+let sleepiness = 0; // 0 から 100 までの数値を代入してください
+
+const isHappy = happiness > 90
+const isSleepy = sleepiness > 90
+
+const isNotHappyAndSleepy = !isHappy && isSleepy
+
+// 以下のコメントアウトを外して確認してみましょう
+// console.log("isNotHappyAndSleepy" + "は" + true + "ですか？", isNotHappyAndSleepy)
+// console.log("isHappy", isHappy)
+// console.log("isSleepy", isSleepy)
